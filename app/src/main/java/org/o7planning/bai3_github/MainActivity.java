9264fc7f76f49d1b4665cea17b1cb8dc0e884604
lethithanhbinh1101 <lethithanhbinh1101@gmail.com>
2020-06-08 09:53:20 +0700

    Initial commit

---
+++ app/src/main/java/org/o7planning/bai3_github/MainActivity.java
@@ -0,0 +1,14 @@
+package org.o7planning.bai3_github;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
